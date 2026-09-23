de32ae5f90dbb1998f5ce7ada87b73335760c2ea
Frank Preuß <preuss.frank.94@gmail.com>
2026-09-23 16:49:34 +0200

    added header and hero section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
+import { Hero } from './components/hero/hero';
+import { About } from './components/about/about';
 
 @Component({
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Hero, About],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
