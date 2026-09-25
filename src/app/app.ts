bafd9c2f10161cd7ace5d52a2b58f27ae325f90c
Frank Preuß <preuss.frank.94@gmail.com>
2026-09-25 13:37:47 +0200

    added skills section and skill Set Text

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
 import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
 
 @Component({
-  imports: [RouterOutlet, Header, Hero, About],
+  imports: [RouterOutlet, Header, Hero, About, Skills],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
