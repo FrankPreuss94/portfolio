82f20a299ecaab488453a6560002fcffae8e1dc2
Frank Preuß <preuss.frank.94@gmail.com>
2026-09-25 14:26:32 +0200

    added skills icons

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -6,4 +6,56 @@ import { Component } from '@angular/core';
   styleUrl: './skills.scss',
   templateUrl: './skills.html',
 })
-export class Skills {}
+export class Skills {
+  skills = [
+    {
+      name: "HTML",
+      src: "./assets/images/tech_icons/HTML.png"
+    },
+    {
+      name: "CSS",
+      src: "./assets/images/tech_icons/CSS.png"
+    },
+    {
+      name: "JavaScript",
+      src: "./assets/images/tech_icons/JavaScript.png"
+    },
+    {
+      name: "Material Design",
+      src: "./assets/images/tech_icons/MaterialDesign.png"
+    },
+    {
+      name: "TypeScript",
+      src: "./assets/images/tech_icons/TypeScript.png"
+    },
+    {
+      name: "Angular",
+      src: "./assets/images/tech_icons/Angular.png"
+    },
+    {
+      name: "Supabase",
+      src: "./assets/images/tech_icons/Supabase.png"
+    },
+    {
+      name: "Git",
+      src: "./assets/images/tech_icons/Git.png"
+    },
+    {
+      name: "REST-API",
+      src: "./assets/images/tech_icons/Rest-Api.png"
+    },
+    {
+      name: "Scrum",
+      src: "./assets/images/tech_icons/Scrum.png"
+    },
+    {
+      name: "Growth mindset",
+      src: "./assets/images/tech_icons/GrowthMindset.png"
+    },
+
+
+
+  ]
+
+
+}
